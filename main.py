fd5c9632b2009e68e5192a6268e0779a4c106155
Andrew4598 <andrijartamonov007@gmail.com>
2026-03-14 12:47:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 #Hello World
 print ("Hello World")
 print ("aiogram is a modern and fully asynchronous framework for Telegram Bot API written in Python 3.9 using asyncio and aiohttp.")
+#I wrote this because I'm working on Python 3.9.
